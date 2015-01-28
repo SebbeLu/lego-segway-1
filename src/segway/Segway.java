6dfee611bca0cee58ead7d381e34ea0b887131a2
Martin Taibr <taibr.martin@gmail.com>
2015-01-28 18:22:50 +0100

    15-01-28

--- src/segway/Segway.java
+++ src/segway/Segway.java
@@ -1,19 +1,33 @@
 package segway;
 
+import java.util.Queue;
+
+import lejos.nxt.BasicMotor;
 import lejos.nxt.LCD;
+import lejos.nxt.Motor;
 import lejos.nxt.MotorPort;
+import lejos.nxt.NXTMotor;
 import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.addon.AccelHTSensor;
 import lejos.nxt.addon.GyroSensor;
+import lejos.robotics.EncoderMotor;
+import lejos.robotics.navigation.Segoway;
 
 public class Segway {
+	final static double MOVING_AVERAGE_PERCENTAGE = 0.6;
+	
 	AccelHTSensor as = new AccelHTSensor(SensorPort.S1);
 	GyroSensor gs = new GyroSensor(SensorPort.S4);
-	NXTRegulatedMotor mr = new NXTRegulatedMotor(MotorPort.B); // + back
-	NXTRegulatedMotor ml = new NXTRegulatedMotor(MotorPort.C); // + back
+	EncoderMotor mr = new NXTMotor(MotorPort.A); // + back
+	EncoderMotor ml = new NXTMotor(MotorPort.C); // + back
 
+	float gyroOffset;
+	//Queue<Float> gyroVals = new Queue<>();
+	double gyroLast;
+	
 	public void run() {
+		// wait for horizontal position
 		if (as.getYAccel() > -180)
 			System.out.println("Wrong position");
 		while (as.getYAccel() > -180) {
@@ -21,11 +35,13 @@ public class Segway {
 		}
 		Utils.sleep(500);
 
+		// find gyro offset
 		float sum = 0;
 		for (int i = 0; i < 10; i++) {
 			sum += gs.getAngularVelocity();
 		}
-		float gyroOffset = sum / 10;
+		gyroOffset = sum / 10;
+		gyroLast = gyroOffset;
 
 		System.out.println("Ready");
 
@@ -36,30 +52,43 @@ public class Segway {
 		PID pid = new PID(5, 0, 0, 0);
 		long t1 = System.currentTimeMillis();
 
-		mr.setSpeed(0);
-		ml.setSpeed(0);
-		mr.forward();
-		ml.forward();
+        mr.setPower(0);
+        ml.setPower(0);
+        mr.forward();
+        ml.forward();
 
+		int x;
+		double vel;
+		long t2;
+		double change;
+		long tUpdate = System.currentTimeMillis();
 		while (true) {
-			int x = as.getXAccel(); // + down
+			x = as.getXAccel(); // + down
 			// int y = as.getYAccel(); // + front
 			// int z = as.getZAccel(); // + robot left
 
-			float vel = gs.getAngularVelocity() - gyroOffset; // + back
-			long t2 = System.currentTimeMillis();
-			double change = -pid.step(t2 - t1, vel);
-			t1 = t2;
-
-			// LCD.clear();
-			// LCD.drawString(Integer.toString(x), 1, 1);
-			// LCD.drawString(Float.toString(vel), 1, 2);
-			// LCD.drawString(Double.toString(change), 1, 3);
-
+			vel = getGyroVal();
+			t2 = System.currentTimeMillis();
+			change = -pid.step(t2 - t1, vel);
 			setMotors((float) change);
 
-			Utils.sleep(50);
+			if (System.currentTimeMillis() - tUpdate > 250) {
+				// LCD.clear();
+				// LCD.drawString(Integer.toString(x), 1, 1);
+				// LCD.drawString(Double.toString(vel), 1, 2);
+				LCD.drawString(Double.toString(change), 1, 3);
+				LCD.drawString(""+System.currentTimeMillis(), 1, 4);
+				tUpdate = System.currentTimeMillis();
+			}
+			
+			t1 = t2;
+			Utils.sleep(1);
 		}
+	}
+
+	public double getGyroVal() {
+		gyroLast = gyroLast * MOVING_AVERAGE_PERCENTAGE + gs.getAngularVelocity() * (1 - MOVING_AVERAGE_PERCENTAGE);
+		return gyroLast - gyroOffset; // + back
 	}
 
 	public void setMotors(float mrls) {
@@ -67,22 +96,18 @@ public class Segway {
 	}
 
 	public void setMotors(float mrs, float mls) {
+		mr.setPower((int) Math.abs(mrs));
 		if (mrs < 0) {
-			mr.setSpeed(Math.abs(mrs));
 			mr.backward();
 		} else if (mrs > 0) {
-			mr.setSpeed(Math.abs(mrs));
 			mr.forward();
-		} else
-			mr.setSpeed(0);
-
+		}
+		
+		ml.setPower((int) Math.abs(mls));
 		if (mls < 0) {
-			ml.setSpeed(Math.abs(mls));
 			ml.backward();
 		} else if (mls > 0) {
-			ml.setSpeed(Math.abs(mls));
 			ml.forward();
-		} else
-			ml.setSpeed(0);
+		}
 	}
 }
--- src/segway/SegwayMain.java
+++ src/segway/SegwayMain.java
@@ -5,6 +5,8 @@ public class SegwayMain {
 	public static void main(String[] args) {
 		Segway segway = new Segway();
 		segway.run();
+//		SegowayPilot sp = new SegowayPilot(new NXTMotor(MotorPort.C), new NXTMotor(MotorPort.B), new GyroSensor(SensorPort.S4), 5.6, 15);
+//		Utils.sleep(100000);
 	}
 
 }
