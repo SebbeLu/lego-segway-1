856b5ea2cd9c85cbcf32ef4d1cb9d63ae1c4c960
Martin Taibr <taibr.martin@gmail.com>
2015-03-25 18:13:55 +0100

    Better code, better sine

--- src/segway/Segway.java
+++ src/segway/Segway.java
@@ -1,5 +1,6 @@
 package segway;
 
+import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.MotorPort;
 import lejos.nxt.NXTMotor;
@@ -12,7 +13,7 @@ import lejos.robotics.EncoderMotor;
 public class Segway {
 	static final double MOVING_AVERAGE_PERCENTAGE = 0.9;
 	static final int AVG_SAMPLES = 20;
-	static final double CORRECT_ANGLE = 85;
+	static final double CORRECT_ANGLE = 85 - 90;
 
 	AccelHTSensor as = new AccelHTSensor(SensorPort.S1);
 	GyroSensor gs = new GyroSensor(SensorPort.S3);
@@ -20,10 +21,10 @@ public class Segway {
 	EncoderMotor ml = new NXTMotor(MotorPort.C); // + back
 
 	double gyroOffset;
-	double gyroLast;
-	double xLast;
-	double yLast;
-	double avgChange = 0;
+	double gyroVel;
+	double accelX;
+	double accelY;
+	double anglePrev;
 
 	public void run() {
 		mr.setPower(0);
@@ -48,17 +49,19 @@ public class Segway {
 		}
 		gyroOffset = sum / AVG_SAMPLES;
 		System.out.println("Offset: " + gyroOffset);
-		gyroLast = gyroOffset;
+		gyroVel = 0;
 		
+		// start
 		System.out.println("Ready");
-		while (as.getXAccel() < 205) { Utils.sleep(50); }
+		//while (as.getXAccel() < 205) { Utils.sleep(50); }
+		Button.waitForAnyPress();
 
-		PID pid = new PID(10000, 100, 0, 0);
+		PID pid = new PID(10, 0, 0, 0);
 		long t1 = System.currentTimeMillis();
 
-		xLast = as.getXAccel();
-		yLast = as.getYAccel();
-		double anglePrev = Math.toDegrees(Math.atan2(xLast, yLast));
+		accelX = as.getXAccel();
+		accelY = as.getYAccel();
+		anglePrev = Math.toDegrees(Math.atan2(accelX, accelY));
 		long t2;
 		double change;
 		long tUpdate = System.currentTimeMillis();
@@ -67,36 +70,27 @@ public class Segway {
 			// x = as.getXAccel(); // + down
 			// y = as.getYAccel(); // + front
 			// int z = as.getZAccel(); // + robot left
+			// gyroVel - front
 			updateAccel();
-			double angleCurr = Math.toDegrees(Math.atan2(xLast, yLast));
-			double changeFromOptimal;
-			if (angleCurr > CORRECT_ANGLE && CORRECT_ANGLE > anglePrev) {
-				changeFromOptimal = CORRECT_ANGLE - angleCurr;
-			} else if (angleCurr < CORRECT_ANGLE && CORRECT_ANGLE < anglePrev) {
-				changeFromOptimal = angleCurr - CORRECT_ANGLE;
-			} else {
-				changeFromOptimal = Math.abs(angleCurr - anglePrev);
-				double dc = Math.abs(CORRECT_ANGLE - angleCurr);
-				double dp = Math.abs(CORRECT_ANGLE - anglePrev);
-				if (dc > dp) {
-					changeFromOptimal *= -1;
-				}
-			}
-			changeFromOptimal *= (double) (t2 - t1) / 1000;			
-			//double angleChange = (angleCurr - anglePrev) / (t2 - t1) * 1000; // - front 
+			updateGyroVel();
+			double angleCurr = Math.toDegrees(Math.atan2(accelX, accelY)) - 90; // front: -
+			double accelChangeFromOptimal = getChangeFromOptimalAccel(angleCurr) * (t2 - t1) / 1000;
 
-			double vel = getGyroVal(); // - front
-			//change = -pid.step(t2 - t1, vel);
-			change = pid.step(t2 - t1, changeFromOptimal) * Math.signum(vel);
+			double toPid = 0;
+			double offset = Math.abs(CORRECT_ANGLE - angleCurr);
+			
+			toPid = angleCurr * offset + gyroVel * (90 - offset);
+			
+			change = pid.step(t2 - t1, 0);
 			setMotors((float) change);
 
 			if (System.currentTimeMillis() - tUpdate > 250) {
 				LCD.clear();
-				LCD.drawString("x  " + xLast, 1, 1);
-				LCD.drawString("v  " + vel, 1, 2);
+				LCD.drawString("x  " + accelX, 1, 1);
+				LCD.drawString("v  " + gyroVel, 1, 2);
 				LCD.drawString("c  " + change, 1, 3);
-				LCD.drawString("a  " + angleCurr, 1, 4);
-				LCD.drawString("co " + changeFromOptimal, 1, 5);
+				LCD.drawString("an " + angleCurr, 1, 4);
+				LCD.drawString("co " + accelChangeFromOptimal, 1, 5);
 				//LCD.drawString("d  " + (vel - angleChange), 1, 6);
 				LCD.drawString("" + System.currentTimeMillis(), 1, 7);
 				tUpdate = System.currentTimeMillis();
@@ -107,30 +101,37 @@ public class Segway {
 			Utils.sleep(1);
 		}
 	}
+	
+	public double getChangeFromOptimalAccel(double angleCurr) {
+		double changeFromOptimal;
+		if (angleCurr > CORRECT_ANGLE && CORRECT_ANGLE > anglePrev) {
+			changeFromOptimal = CORRECT_ANGLE - angleCurr;
+		} else if (angleCurr < CORRECT_ANGLE && CORRECT_ANGLE < anglePrev) {
+			changeFromOptimal = angleCurr - CORRECT_ANGLE;
+		} else {
+			changeFromOptimal = Math.abs(angleCurr - anglePrev);
+			double dc = Math.abs(CORRECT_ANGLE - angleCurr);
+			double dp = Math.abs(CORRECT_ANGLE - anglePrev);
+			if (dc > dp) {
+				changeFromOptimal *= -1;
+			}
+		}
+		return changeFromOptimal;
+	}
 
-	public double getGyroVal() {
-		gyroLast = gyroLast * MOVING_AVERAGE_PERCENTAGE + gs.getAngularVelocity() * (1 - MOVING_AVERAGE_PERCENTAGE);
-		//gyroLast = (100 * gyroLast + gs.getAngularVelocity()) / 101;
-		return gyroLast - gyroOffset; // + back
+	public void updateGyroVel() {
+		gyroVel = gyroVel * MOVING_AVERAGE_PERCENTAGE +
+				(gs.getAngularVelocity() - gyroOffset) * (1 - MOVING_AVERAGE_PERCENTAGE);
 	}
 	
 	public void updateAccel() {
-		xLast = xLast * MOVING_AVERAGE_PERCENTAGE + as.getXAccel() * (1 - MOVING_AVERAGE_PERCENTAGE);
-		yLast = yLast * MOVING_AVERAGE_PERCENTAGE + as.getYAccel() * (1 - MOVING_AVERAGE_PERCENTAGE);
+		accelX = accelX * MOVING_AVERAGE_PERCENTAGE + as.getXAccel() * (1 - MOVING_AVERAGE_PERCENTAGE);
+		accelY = accelY * MOVING_AVERAGE_PERCENTAGE + as.getYAccel() * (1 - MOVING_AVERAGE_PERCENTAGE);
 	}
 
-	/*public double getAngleChange(double change) {
-		//avgChange = avgChange * MOVING_AVERAGE_PERCENTAGE + change * (1 - MOVING_AVERAGE_PERCENTAGE);
-		//avgChange = (avgChange + change) / 2;
-		
-		System.out.println((int)((avgChange + change) / 2)+" "+change+" "+(avgChange+change));
-		return avgChange;
-	}*/
-
 	public void setMotors(double mrls) {
-		setMotors(mrls + 5*Math.sin(System.currentTimeMillis()/100),
-				mrls + 5*Math.sin(System.currentTimeMillis()/100 + Math.PI));
-		setMotors(mrls, mrls);
+		setMotors(mrls + 18*Math.sin(System.currentTimeMillis()/(double)40),
+				mrls + 18*Math.sin(System.currentTimeMillis()/(double)40 + Math.PI));
 	}
 
 	public void setMotors(double mrs, double mls) {
